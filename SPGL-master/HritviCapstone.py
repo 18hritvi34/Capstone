bd8fb50d6707f19e1890d24c6d6cc3b703dcd96c
18hritvi34 <37602243+18hritvi34@users.noreply.github.com>
2018-04-20 12:57:51 +0900

    Add files via upload

--- SPGL-master/HritviCapstone.py
+++ SPGL-master/HritviCapstone.py
@@ -10,17 +10,7 @@ import turtle
 import random
 
 # Initial Game setup
-game = spgl.Game(950, 700, "white", "Filler", 0)
-
-# Set up screen 
-wn = turtle.Screen()
-wn.register_shape("title.gif")
-wn.register_shape("orange.gif")
-wn.register_shape("yellow.gif")
-wn.register_shape("turquoise.gif")
-wn.register_shape("blue.gif")
-wn.register_shape("cream.gif")
-wn.register_shape("purple.gif")
+game = spgl.Game(1100, 700, "white", "Filler", 0)
 
 # List for the colors
 colors = ["O", "Y", "T", "B", "P", "C"]
@@ -31,53 +21,7 @@ class Title(spgl.Sprite):
 	def __init__ (self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 		self.shape("title.gif")
-		self.goto(215.0,245.0)
-		
-class Orange(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("orange.gif")
-		self.speed = 0 
-	
-	def click(self):
-		print("Orange Clicked")
-
-class Yellow(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("yellow.gif")
-		self.speed = 0
-		
-	def click(self):
-		print("Yellow Clicked")
-		
-class Turquoise(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("turquoise.gif")
-		self.goto(164, 150)
-		self.speed = 0
-
-class Blue(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("blue.gif")
-		self.goto(214, 150)
-		self.speed = 0
-
-class Purple(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("purple.gif")
-		self.goto(264, 150) 
-		self.speed = 0
-
-class Cream(spgl.Sprite):
-	def __init__(self, shape, color, x, y):
-		spgl.Sprite.__init__(self, shape, color, x, y)
-		self.shape("cream.gif")
-		self.goto(314, 150)
-		self.speed = 0
+		self.goto(280.0,220.0)
 		
 class Grid():
 	def __init__(self, squares, rows, columns, x, y):
@@ -110,8 +54,8 @@ class Grid():
 
 		for row in range(6):
 			for column in range(6):
-				screen_x = -300 + column * 35
-				screen_y = 150 - row * 40
+				screen_x = -400 + column * 70
+				screen_y = 200 - row * 80
 				self.pen.goto(screen_x, screen_y)
 				#select color
 				if grid[row][column] == "O":
@@ -126,26 +70,85 @@ class Grid():
 					self.pen.shape("purple.gif")
 				if grid[row][column] == "Y":
 					self.pen.shape("yellow.gif")
-				
-				
-				self.pen.stamp()	
+				self.pen.stamp()
+					
+		#def click(self, x, y):
+			#print("Orange Clicked")
+			#grid[row].append("orange.gif")
+
 			
+				
+				
+				
+					
+class Orange(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("orange.gif")
+		self.speed = 0 
+	
+	
+
+class Yellow(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("yellow.gif")
+		self.speed = 0
 		
 	def click(self, x, y):
-		pass
+		print("Yellow Clicked")
+		
+class Turquoise(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("turquoise.gif")
+		self.speed = 0
+		
+	def click(self, x, y):
+		print("Turquoise Clicked")
+
+class Blue(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("blue.gif")
+		self.speed = 0
+	
+	def click(self, x, y):
+		print("Blue Clicked")
+
+class Purple(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("purple.gif")
+		self.speed = 0
+	
+	def click(self, x, y):
+		print("Purple Clicked")
+
+class Cream(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("cream.gif")
+		self.speed = 0
+	
+	def click(self, x, y):
+		print("Cream Clicked")
+	
+				 
+			 
 		
 		
 		
 		 
 # Create Instances 
 
-title = Title("square", "blue", 300.0,250.0)
-orange = Orange("square", "orange", 64, 150)
-yellow = Yellow("square", "red", 114, 150)
-turq = Turquoise("square", "yellow", 164, 150)
-blue = Blue("square", "green", 214, 150)
-cream = Cream("square", "blue", 314, 150)
-purple = Purple("square", "pink", 264, 150)
+title = Title("title.gif", "blue", 215.0, 220.0)
+orange = Orange("orange.gif", "orange", 50, 100)
+yellow = Yellow("yellow.gif", "red", 130, 100)
+turq = Turquoise("turquoise.gif", "yellow", 210, 100)
+blue = Blue("blue.gif", "green", 290, 100)
+cream = Cream("cream.gif", "blue", 370, 100)
+purple = Purple("purple.gif", "pink", 450, 100)
 grid = Grid(36, 6, 6, -300.0,150.0) 
 
 # Create Sprites
@@ -156,6 +159,7 @@ grid = Grid(36, 6, 6, -300.0,150.0)
 
 
 # Set Mousepad bindings 
+
 
 while True:
     # Call the game tick method
